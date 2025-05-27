9ed838f76e2241ac5116099105b8d83890b42914
Pablo Mendez <pmendezpuentes@gmail.com>
2025-05-26 22:15:37 -0500

    Se agregan los metodos agregar, editar, eliminar

--- src/main/java/pm/contactos/servicio/ContactoServicio.java
+++ src/main/java/pm/contactos/servicio/ContactoServicio.java
@@ -1,11 +1,11 @@
 package pm.contactos.servicio;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pm.contactos.modelo.Contacto;
 import pm.contactos.repositorio.ContactoRepositorio;
-
 import java.util.List;
 
+@Service
 public class ContactoServicio implements IContactoServicio{
 
     @Autowired
